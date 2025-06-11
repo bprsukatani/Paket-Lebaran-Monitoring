e81a765f64033fdd396957c472dfbdf140b6b568
bprsukatani <sukatanibpr@gmail.com>
2025-06-11 15:33:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // Konfigurasi Firebase Anda
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY", // Ganti dengan API Key Anda
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com", // Ganti dengan Project ID Anda
-  projectId: "YOUR_PROJECT_ID", // Ganti dengan Project ID Anda
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyBFzJr48Y8F3_S_d6t_Qlg3NinlyLvFiGU",
+  authDomain: "bpr-monitoring.firebaseapp.com",
+  projectId: "bpr-monitoring",
+  storageBucket: "bpr-monitoring.firebasestorage.app",
+  messagingSenderId: "787451089590",
+  appId: "1:787451089590:web:23e008144972a5cecc0c17",
+  measurementId: "G-SNE62EWQVL"
 };
 
 // Inisialisasi Firebase
